3229362e07393d741fc90ddf5abec14e8ead217e
scott1028 <mic1028002@gmail.com>
2013-09-03 14:45:14 +0800

    modified:   a.exe 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 
 // Windows 版本
-// c++ -I ../../Desktop/boost_1_54_0 .\main.cpp
+// c++ -I../../Desktop/boost_1_54_0 .\main.cpp
 
 int main(){
 	using namespace boost::lambda;
@@ -14,3 +14,25 @@ int main(){
 		in(std::cin), in(), std::cout << (_1 * 3) << " "
 	);
 }
+
+/*
+	#include <boost/regex.hpp>
+	#include <iostream>
+	#include <string>
+
+	// Windows 版本
+	// c++ -I ../../Desktop/boost_1_54_0 .\main.cpp
+
+	int main(){
+		std::string line;
+		boost::regex pat( "^Subject: (Re: |Aw: )*(.*)" );
+
+		while (std::cin)
+		{
+			std::getline(std::cin, line);
+			boost::smatch matches;
+			if (boost::regex_match(line, matches, pat))
+				std::cout << matches[2] << std::endl;
+		}
+	}
+*/
